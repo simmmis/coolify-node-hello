cadea87faedd18b4e0f3fd40df187a139ff630bd
Dmitry <simmmis@gmail.com>
2025-04-26 12:14:49 -0500

    Update index.js

--- index.js
+++ index.js
@@ -3,14 +3,16 @@ const app = express();
 const PORT = process.env.PORT || 3080;
 
 app.get('/', (req, res) => {
-  res.send(`
+  res.send(`<pre>
            Hello from Coolify 👋 | 
            this is automatical deployment system check <br> РАБОТАЕТ!!!!! УРАААА
            тут переносы строк и прочие прелести
            
            вот так
            
-           ----`);
+           ----
+           вот
+           </pre>`);
 });
 
 app.listen(PORT, () => {
